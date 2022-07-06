1ebe07b2cc226d8128cc1f79cd36266bdde5a961
Jason <haojue@gmail.com>
2022-07-06 22:43:15 +0800

    input html output md

---
+++ main.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+/*
+ * usage: cat test.html | main.js
+ */
+var fs = require('fs');
+var TurndownService = require('turndown')
+
+var turndownService = new TurndownService({
+  codeBlockStyle: 'fenced',
+  headingStyle: 'atx',
+  preformattedCode: 'true'
+})
+turndownService.addRule('fencedCodeBlock', {
+  filter: function (node, options) {
+    return (
+      options.codeBlockStyle === 'fenced' &&
+      node.nodeName === 'PRE' &&
+      node.firstChild &&
+      node.firstChild.nodeName === 'CODE'
+    )
+  },
+
+  replacement: function (content, node, options) {
+
+    var className = node.getAttribute('class') || ''
+    var language = (className.match(/language-(\S+)/) || [null, ''])[1]
+    return (
+      '\n\n' + options.fence + language + '\n' +
+      node.firstChild.textContent +
+      '\n' + options.fence + '\n\n'
+    )
+  }
+})
+// var content = fs.readFileSync('test.html', { encoding: 'utf8', flag: 'r' })
+var stdinBuffer = fs.readFileSync(0)
+var markdown = turndownService.turndown(stdinBuffer.toString())
+console.log(markdown)
